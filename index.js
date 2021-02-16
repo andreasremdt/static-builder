fda56bf26c728483db555902021ff7cbcc9dc5b6
Andreas Remdt <me@andreasremdt.com>
2021-02-16 17:19:32 +0100

    feat: parse templates with nunjucks

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var fs = require("fs").promises;
 var path = require("path");
 var marked = require("marked");
+var nunjucks = require("nunjucks");
 
 const TEMPLATE_FOLDER = path.join(__dirname, "testing");
 const OUTPUT_FOLDER = path.join(__dirname, "output");
 const VALID_EXTENSIONS = [".md", ".markdown", ".html"];
 const MARKDOWN_EXTENSIONS = [".md", ".markdown"];
+
+nunjucks.configure(["testing/includes", "testing/layouts"], {
+  trimBlocks: true,
+  lstripBlocks: true,
+});
 
 (async () => {
   var files = await fs.readdir(TEMPLATE_FOLDER);
@@ -25,6 +31,17 @@ const MARKDOWN_EXTENSIONS = [".md", ".markdown"];
     if (MARKDOWN_EXTENSIONS.includes(path.extname(page))) {
       page = page.replace(/.(md|markdown)$/, ".html");
       content = marked(content);
+    }
+
+    try {
+      content = nunjucks.renderString(content);
+    } catch (ex) {
+      var stack = ex.message.split(/\n/g);
+      return console.error(
+        `Could not create \`${page}\`, ${stack[stack.length - 1].slice(
+          9
+        )}. This file will be skipped.`
+      );
     }
 
     await fs.writeFile(path.join(OUTPUT_FOLDER, page), content);
